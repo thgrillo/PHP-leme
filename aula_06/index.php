12a07cedbe5ab9c8a20919bba7308b63bf16f95f
Thiago Grillo <thiago_grillo@hotmail.com>
2022-04-26 13:36:02 -0300

    Aula 6 e 7

---
+++ aula_06/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$valor = 3;
+
+switch ($valor){
+
+    case "1":
+        echo "Bom dia";
+    break;
+    case "2":
+        echo "Boa tarde";
+    break;
+    case "3":
+        echo "Boa noite";
+    break;
+    default:
+        echo "bom dia";
+}
