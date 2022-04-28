30823ac17437e1f50480ddf68ca2fd07c02791eb
Thiago Grillo <thiago_grillo@hotmail.com>
2022-04-28 10:24:44 -0300

    json to array

---
+++ secao_04/aula_03/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$jsonstr = '{"title":"FilmeA","sinopse":"lorem ipsumn bla blabla","ano":1998,"genero":"comedia","canais":["globo","netflix","sbt"]}';
+
+$arrFilme = json_decode($jsonstr);
+
+//var_dump($arrFilme);
+
+echo "<p><b>Titulo:</b> ". $arrFilme->title ." </p>";
+echo "<p><b>Sinopse:</b> ". $arrFilme->{"sinopse"} ."</p>";
+echo "<p><b>Ano:</b> ". $arrFilme->ano ."</p>";
+echo "<p><b>Genero:</b> ". $arrFilme->genero ."</p>";
+echo "<p><b>Canais:</b> </p>";
+
+
+//pra mostrar o array precisa de um FOR
+//echo "<p><b>Canais:</b> ". $arrFilme->canais ."</p>";
+
+for ($i=0; $i < count($arrFilme->canais); $i++) { 
+    echo "<p>". $arrFilme->canais[$i] ."</p>";
+}
